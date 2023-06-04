45067722216ca3e2b8c39a75afcb106343442386
VuVanHoc <hocvanvu1999@gmail.com>
2023-06-04 07:44:19 +0700

    fix router error

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Header from "./components/header";
+import Sidebar from "./components/sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +14,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body suppressHydrationWarning={true} className={inter.className}>
-        {children}
+        <main className="flex">
+          <Sidebar />
+          <div>
+            <Header />
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,3 @@
-import Header from "./components/header";
-import Sidebar from "./components/sidebar";
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen px-24">
-      <Sidebar />
-      <Header />
-    </main>
-  );
+  return <section className="flex min-h-screen px-24"></section>;
 }
